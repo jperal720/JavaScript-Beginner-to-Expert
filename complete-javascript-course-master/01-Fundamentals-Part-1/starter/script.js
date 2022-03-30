023a152087ad881acdb3659d7c7a108fad8da6e7
Jonathan <jperalgo@sfu.ca>
2022-03-30 11:30:06 -0700

    Finished Coding challenge #1

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,37 @@
-let js = "azing";
-console.log(40 + 8 - 10);
+// let js = "azing";
+// console.log(40 + 8 - 10);
 
-let firstName = "Jonas";
+// let firstName = "Jonas";
 
-console.log(firstName, "thing");
+// console.log(firstName, "thing");
 
-const currentYear = 2022;
+// const currentYear = 2022;
 
-const birthYearJohn = 1992;
-const birthYearSarah = 1993;
+// const birthYearJohn = 1992;
+// const birthYearSarah = 1993;
 
-const ageJohn = currentYear - birthYearJohn;
-const ageSarah = currentYear - birthYearSarah;
+// const ageJohn = currentYear - birthYearJohn;
+// const ageSarah = currentYear - birthYearSarah;
 
-const sarahIsLegal = ageSarah >= 19;
+// const sarahIsLegal = ageSarah >= 19;
 
 
-console.log(ageJohn >= ageSarah);
+// console.log(ageJohn >= ageSarah);
+
+
+//Coding Challenge #1
+
+const weightMark = 78; //in kg
+const weightJohn = 92;
+
+const heightMark = 1.69; //in meters
+const heightJohn = 1.95;
+
+const bmiMark = weightMark / (heightMark ** 2);
+const bmiJohn = weightJohn / (heightJohn ** 2);
+
+const markHigherBMI = bmiMark > bmiJohn;
+
+console.log("Mark's BMI: " + bmiMark);
+console.log("John's BMI: " + bmiJohn);
+console.log("Mark has a higher BMI than John: " + markHigherBMI)
