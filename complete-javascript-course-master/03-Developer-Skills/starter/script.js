f766403ad72752c2acd578fc8459eb309ce7146b
Jonathan <jperalgo@sfu.ca>
2022-04-04 12:04:34 -0700

    Finished coding challenge #1

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -45,18 +45,58 @@
 // console.log(
 //   `The temperature amplitude is ${calcAmplitude(temperature1, temperature2)}`
 // );
-const measureKelvin = function () {
-  const measurement = {
-    type: 'temperature',
-    unit: 'celsius',
-    value: Number(prompt('Degrees celsius')),
-  };
 
-  //   console.table(measurement); //Handy for displaying objects in the console
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temperature',
+//     unit: 'celsius',
+//     // value: Number(prompt('Degrees celsius')),;
+//     value: 10,
+//   };
 
-  const kelvin = measurement.value + 273;
+//   //   console.table(measurement); //Handy for displaying objects in the console
 
-  return kelvin;
+//   const kelvin = measurement.value + 273;
+
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());
+
+// const temperature1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const temperature2 = [-22, 2, 14, 20];
+
+// const calcAmplitudeBug = function (temperature1, temperature2) {
+//   const temperature = temperature1.concat(temperature2);
+//   console.log(temperature);
+//   let highest;
+//   let lowest;
+
+//   debugger;
+
+//   for (let i = 0; i < temperature.length; i++) {
+//     let currentTemp = temperature[i];
+//     if (typeof currentTemp !== 'number') continue;
+//     else if (highest == null && lowest == null)
+//       (highest = currentTemp), (lowest = currentTemp);
+//     else if (currentTemp > highest) highest = currentTemp;
+//     else if (currentTemp < lowest) lowest = currentTemp;
+//   };
+
+//   return highest - lowest;
+// };
+
+// console.log(
+//   `The temperature amplitude is ${calcAmplitudeBug(temperature1, temperature2)}`
+// );
+
+const data = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) str += `${arr[i]}°C in ${i + 1} days...`;
+
+  console.log(str);
 };
 
-console.log(measureKelvin());
+printForecast(data);
