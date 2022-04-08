5d1c9824798e7f1e5f78f50576971b029a49f448
Jonathan <jperalgo@sfu.ca>
2022-04-08 11:59:48 -0700

    Learned how to do Event Listeners and putting Event Handlers inside them (Fucntions)

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,13 @@
-'use strict';
+// 'use strict';
 
-const p = document.querySelector('.message');
+// const p = document.querySelector('.message');
 
-console.log(document.querySelector('.message').textContent); //To select elements from HTML
+// console.log(document.querySelector('.message').textContent); //To select elements from HTML
+
+document.querySelector('.check').addEventListener('click', function () {
+  const userInput = document.querySelector('.guess').valueAsNumber;
+
+  if (userInput == NaN) return;
+
+  console.log(userInput);
+});
