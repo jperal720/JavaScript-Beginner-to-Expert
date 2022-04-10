232d1312a90d40f00a58a705b9e4ffc3b7bfb4f5
Jonathan <jperalgo@sfu.ca>
2022-04-09 21:03:34 -0700

    Refactored compare conditions

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -4,8 +4,6 @@ const highScore = document.querySelector('.highscore');
 const message = document.querySelector('.message');
 let gameFinished = false; //displays whether game is finished or not
 
-// document.querySelector('.message').textContent = 'this is the new message';
-
 document.querySelector('.check').addEventListener('click', function () {
   const userInput = document.querySelector('.guess').valueAsNumber;
 
@@ -17,40 +15,39 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   number = message.textContent = 'Start guessing...';
   score.textContent = '20';
-  number = Math.trunc(Math.random() * 20);
+  number = Math.trunc(Math.random() * 20) + 1;
   gameFinished = false;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
 });
 
 const compare = function (userGuess) {
   if (userGuess == number) finished(); //Case in which user wins!
-  else if (
-    //Case in which user loses!
-    score.textContent <= 1 &&
-    (userGuess > number || userGuess < number)
-  ) {
-    score.textContent -= 1;
-    finished();
-  } else if (userGuess > number) {
-    message.textContent = 'Guess is too high!';
-    score.textContent -= 1;
-  } else if (userGuess < number) {
-    message.textContent = 'Guess is too low!';
-    score.textContent -= 1;
+  else {
+    if (score.textContent <= 1) {
+      //Case in which user loses!
+      score.textContent--;
+      finished();
+    } else {
+      message.textContent =
+        userGuess > number ? 'Guess is too high!' : 'Guess is too low!';
+      score.textContent--;
+    }
   }
 };
 
 const finished = function () {
   gameFinished = true;
-  document.querySelector('body').style.backgroundColor = '#60b347';
   document.querySelector('.number').textContent = number;
   document.querySelector('.number').style.width = '30rem';
   if (score.textContent == 0) {
     message.textContent = 'Sorry, you ran out of tries :(';
+    document.querySelector('body').style.backgroundColor = '#D24040';
     return;
   }
+  document.querySelector('body').style.backgroundColor = '#60b347';
   message.textContent = 'Congratulations you guessed the number!';
   if (Number(score.textContent) > Number(highScore.textContent))
     highScore.textContent = score.textContent;
