3fd94c076a2f366d9a42cdcdd421676d0a8d16f1
Jonathan <jperalgo@sfu.ca>
2022-04-11 14:55:41 -0700

    We will begin to implement the fetch api, instead of the get api

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -5,37 +5,59 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (name) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
-  request.send();
+// const renderCountry = function (data, className = '') {
+//   const html = `<article class="country ${className}">
+//   <img class="country__img" src="${data.flags.png}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name.official}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       data.population / 1000000
+//     ).toFixed(1)} Million</p>
+//     <p class="country__row"><span>🗣️</span>${
+//       Object.values(data.languages)[0]
+//     }</p>
+//     <p class="country__row"><span>💰</span>${
+//       Object.values(data.currencies)[0].name
+//     }</p>
+//   </div>
+// </article>`;
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-    const html = `<article class="country">
-  <img class="country__img" src="${data.flags.png}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name.official}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      data.population / 1000000
-    ).toFixed(1)} Million</p>
-    <p class="country__row"><span>🗣️</span>${
-      Object.values(data.languages)[0]
-    }</p>
-    <p class="country__row"><span>💰</span>${
-      Object.values(data.currencies)[0].name
-    }</p>
-  </div>
-</article>`;
+// const getCountryAndNeighbor = function (name) {
+//   //AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
+//   request.send();
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
-};
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-getCountryData('cuba');
-getCountryData('chile');
-getCountryData('japan');
+//     //Render country
+//     renderCountry(data);
+
+//     //Get Neighbor Country
+//     const [neighbor] = data.borders;
+
+//     if (!neighbor) return;
+
+//     const requestNeighbor = new XMLHttpRequest();
+//     requestNeighbor.open(
+//       'GET',
+//       `https://restcountries.com/v3.1/alpha/${neighbor}`
+//     );
+//     requestNeighbor.send();
+
+//     requestNeighbor.addEventListener('load', function () {
+//       const [data] = JSON.parse(this.responseText);
+
+//       renderCountry(data, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryAndNeighbor('russia');
