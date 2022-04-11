fdb095953e255dd718456f91ece271714cba957c
Jonathan <jperalgo@sfu.ca>
2022-04-11 13:00:36 -0700

    Implemented basic AJAX

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,38 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function (name) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      data.population / 1000000
+    ).toFixed(1)} Million</p>
+    <p class="country__row"><span>🗣️</span>${
+      Object.values(data.languages)[0]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      Object.values(data.currencies)[0].name
+    }</p>
+  </div>
+</article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('cuba');
+getCountryData('chile');
+getCountryData('japan');
