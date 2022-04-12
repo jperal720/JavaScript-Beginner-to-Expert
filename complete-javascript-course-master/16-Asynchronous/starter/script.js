36b88f32ae8a3b8491ecc56b4c9da596f71d3a05
Jonathan <jperalgo@sfu.ca>
2022-04-12 09:55:22 -0700

    Simplified fetch

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -5,27 +5,27 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-// const renderCountry = function (data, className = '') {
-//   const html = `<article class="country ${className}">
-//   <img class="country__img" src="${data.flags.png}" />
-//   <div class="country__data">
-//     <h3 class="country__name">${data.name.official}</h3>
-//     <h4 class="country__region">${data.region}</h4>
-//     <p class="country__row"><span>👫</span>${(
-//       data.population / 1000000
-//     ).toFixed(1)} Million</p>
-//     <p class="country__row"><span>🗣️</span>${
-//       Object.values(data.languages)[0]
-//     }</p>
-//     <p class="country__row"><span>💰</span>${
-//       Object.values(data.currencies)[0].name
-//     }</p>
-//   </div>
-// </article>`;
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      data.population / 1000000
+    ).toFixed(1)} Million</p>
+    <p class="country__row"><span>🗣️</span>${
+      Object.values(data.languages)[0]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      Object.values(data.currencies)[0].name
+    }</p>
+  </div>
+</article>`;
 
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 // const getCountryAndNeighbor = function (name) {
 //   //AJAX call country 1
@@ -61,3 +61,15 @@ const countriesContainer = document.querySelector('.countries');
 // };
 
 // getCountryAndNeighbor('russia');
+
+const request = fetch(`https://restcountries.com/v3.1/name/cuba`);
+
+console.log(request);
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('cuba');
