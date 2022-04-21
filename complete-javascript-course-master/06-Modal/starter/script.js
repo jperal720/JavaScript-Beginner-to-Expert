1181c58a4457b12093dda278e34d72e879c9edbd
Jonathan <jperalgo@sfu.ca>
2022-04-20 22:47:32 -0700

    Project finished; learned how to check for contained classes in components

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -8,10 +8,12 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 const openModal = function () {
+  if (!modalWindow.classList.contains('hidden')) return;
   modalWindow.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 const closeModal = function () {
+  if (modalWindow.classList.contains('hidden')) return;
   modalWindow.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -23,8 +25,8 @@ overlay.addEventListener('click', closeModal);
 btnCloseModal.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (event) {
-  console.log(event);
   if (event.key === 'Escape') {
+    if (modalWindow.classList.contains('hidden')) return;
     modalWindow.classList.add('hidden');
     overlay.classList.add('hidden');
   }
