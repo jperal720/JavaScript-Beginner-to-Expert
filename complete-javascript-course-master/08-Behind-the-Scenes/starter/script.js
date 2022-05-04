745a8a093f6b65253fa0de8e0cd228d77505f19d
jperalgo <jperalgo@sfu.ca>
2022-05-04 15:12:42 -0700

    Expanding on the arrow function and this functionality

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -6,14 +6,12 @@
 //    * Checking if keyword var ignores scope on functions as well
 //    * !I already know it ignores scope inside blocks
 //    */
-  
+
 //   const variable2 = function(){
 //     var car = true;
 //   }
 
 //   variable2();
-
-  
 
 //   console.log(`car: ${car}`);
 //   return n2;
@@ -40,5 +38,31 @@
 
 // calcAge(1992);
 
+var firstName = 'Matilda';
 
+const jonathan = {
+  firstName: 'Jonathan',
+  lastName: 'Peral',
+  birthYear: 2000,
+  calcAge: function () {
+    console.log("we're good");
+    const birthYear = 2001;
 
+    const self = this; //* by declaring self as this, which points to the global object here.
+    const isMillenial = function () {
+      return self.birthYear >= 1981 && self.birthYear <= 1996 ? true : false;
+    };
+    isMillenial();
+
+    return 2037 - this.birthYear;
+  },
+
+  //!When accessing a property inside an object, that does not exist in that object we get 'undefined'
+  greet: () => console.log(`Hey, I'm ${this.firstName}!`),
+};
+
+//*User Ctrl + Alt + W , and W, once again, to console.log() a variable that you're currently selecting!
+jonathan.calcAge();
+jonathan.greet();
+
+console.log('firstName:', firstName);
