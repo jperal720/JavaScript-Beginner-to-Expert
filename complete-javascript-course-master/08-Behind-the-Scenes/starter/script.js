a13d52a75b4f6902fc8dbaeabb96e28d8689c9ed
jperalgo <jperalgo@sfu.ca>
2022-05-04 15:26:23 -0700

    Learned about arguments keyword, and how it cannot be used in arrow functions

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -40,29 +40,67 @@
 
 var firstName = 'Matilda';
 
-const jonathan = {
-  firstName: 'Jonathan',
-  lastName: 'Peral',
-  birthYear: 2000,
-  calcAge: function () {
-    console.log("we're good");
-    const birthYear = 2001;
+// const jonathan = {
+//   firstName: 'Jonathan',
+//   lastName: 'Peral',
+//   birthYear: 2000,
+//   calcAge: function () {
+//     console.log("we're good");
+//     const birthYear = 2001;
 
-    const self = this; //* by declaring self as this, which points to the global object here.
-    const isMillenial = function () {
-      return self.birthYear >= 1981 && self.birthYear <= 1996 ? true : false;
-    };
-    isMillenial();
+//     const self = this; //* by declaring self as this, which points to the global object here.
+//     const isMillenial = function () {
+//       return self.birthYear >= 1981 && self.birthYear <= 1996 ? true : false;
+//     };
+//     isMillenial();
 
-    return 2037 - this.birthYear;
-  },
+//     return 2037 - this.birthYear;
+//   },
 
-  //!When accessing a property inside an object, that does not exist in that object we get 'undefined'
-  greet: () => console.log(`Hey, I'm ${this.firstName}!`),
+// const jonathan = {
+//   firstName: 'Jonathan',
+//   lastName: 'Peral',
+//   birthYear: 2000,
+//   calcAge: function () {
+//     console.log("we're good");
+//     const birthYear = 2001;
+
+//     // const self = this; //* by declaring self as this, which points to the global object here.
+//     const isMillenial = () => {
+//       console.log(this);
+//       return this.birthYear >= 1981 && this.birthYear <= 1996 ? true : false;
+//     };
+//     isMillenial();
+
+//     return 2037 - this.birthYear;
+//   },
+
+//   //!When accessing a property inside an object, that does not exist in that object we get 'undefined'
+//   greet: () => console.log(`Hey, I'm ${this.firstName}!`),
+// };
+
+// //*User Ctrl + Alt + W , and W, once again, to console.log() a variable that you're currently selecting!
+// jonathan.calcAge();
+// jonathan.greet();
+
+// console.log('firstName:', firstName);
+
+/**
+ * *This function can take more arguments than the ones it supports,
+ * *we access them through the keyword 'arguments'
+ * !This can only be done in a function declaration- it cannot be done in arrow functions.
+ * @param {} a
+ * @param {*} b
+ * @returns
+ */
+const addExpression = function (a, b) {
+  console.log('arguments:', arguments[2]);
+  return a + b;
 };
 
-//*User Ctrl + Alt + W , and W, once again, to console.log() a variable that you're currently selecting!
-jonathan.calcAge();
-jonathan.greet();
+addExpression(2, 5);
+addExpression(2, 5, 8, 10);
 
-console.log('firstName:', firstName);
+const addArrow = (a, b) => a + b;
+
+addArrow(2, 5);
