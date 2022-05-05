0ae116fdf0b6737e949c0848bf34c5dbb20b31c3
jperalgo <jperalgo@sfu.ca>
2022-05-05 16:46:14 -0700

    Learned destructuring of arrays

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,28 @@ const restaurant = {
     },
   },
 };
+
+// const arr = [2, 3, 4];
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// // console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// //* Destructuring array, in order to switch the order.
+// [main, secondary] = [secondary, main];
+
+// console.log(main, secondary);
+
+const [starter, main] = restaurant.order(2, 1);
+
+console.log(starter, main);
+
+const nestedArray = [2, 4, [5, 6]];
+const [i, , [x, y]] = nestedArray;
+
+console.log(i, x, y);
