b028b2b54850ebf95cda14fba4595da872bd811b
jperalgo <jperalgo@sfu.ca>
2022-05-06 02:00:18 -0700

    Learned how to give default values to destructured variables. Also, learned how t destructure parameters passed in a function - caution: names of parameters must be the same as the objects passed when calling the function

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -30,7 +30,22 @@ const restaurant = {
       close: 24,
     },
   },
+
+  //*Here we destrcture in the function parameters ahead of time
+  //!Caution: Make sure names of the parameters are the same as the properties of the object that you are passing into the function
+  //!If we're not sure, we can set default values, for good measure - we can also change the name of the passed properties, if we wish
+  orderReceived: function ({ time: t = '00:00', name: n = 'John Doe' }) {
+    console.log(
+      `${n} just made an order at ${this.name}; his order came in at ${t}`
+    );
+  },
 };
+
+restaurant.orderReceived({
+  names: 'Jonathan Peral',
+  time: '01:52',
+  sex: 'Male',
+});
 
 // const arr = [2, 3, 4];
 // const [x, y, z] = arr;
@@ -68,7 +83,57 @@ const restaurant = {
 //! Caution: when destructuring objects, you MUST make sure that the name of the variables are the same
 //! as the names of the properties in the object; otherwise, they will be 'undefined'.
 
-const { name, openingHours, categories } = restaurant;
-console.log('name:', name);
-console.log('openingHours:', openingHours);
-console.log('categories:', categories);
+// const { name, openingHours, categories } = restaurant;
+// console.log('name:', name);
+// console.log('openingHours:', openingHours);
+// console.log('categories:', categories);
+
+// //!In case we want the variables to have different names from the properties, this is how we do so:
+// const { name: restName, openingHours: opHours, categories: cat } = restaurant;
+// console.log('restName:', restName);
+// console.log('opHours:', opHours);
+// console.log('cat:', cat);
+
+// //* Setting default values in destructures
+// //!Highly useful for when we're not sure if a property is in the object!
+// const {
+//   menu: menu2 = [],
+//   starterMenu: starter = [],
+//   categories: cat2,
+// } = restaurant;
+// console.log('menu:', menu2);
+// console.log('starterMenu:', starter);
+// console.log('cat2:', cat2);
+
+//*Mutating variables
+const {
+  menu: menu2 = [],
+  starterMenu: starter = [],
+  categories: cat2,
+} = restaurant;
+
+let a = 11;
+let b = 99;
+const obj = { a: 23, b: 7, c: 14 };
+
+console.log('Before mutation:');
+console.log('a:', a);
+console.log('b:', b);
+
+({ a, b } = obj);
+
+console.log('After mutation:');
+console.log('a:', a);
+console.log('b:', b);
+
+//*Destructuring objects inside of objects
+const {
+  openingHours: {
+    fri: { open: o, close: c },
+  },
+} = restaurant;
+
+console.log('open:', o);
+console.log('close:', c);
+console.log('oneError:', oneError);
+console.log('two:', two);
