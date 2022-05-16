e8d59714cd2dd79eef9cf0168ac5e428b358c980
jperalgo <jperalgo@sfu.ca>
2022-05-15 21:32:02 -0700

    Learned how to set default parameters

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const bookings = [];
+
+//Default parameters
+const createBooking = function (
+  flightNum = 'DE',
+  numPassenger = 2,
+  price = 199 * numPassenger //This only works when the param have been declared beforehand
+) {
+  //   //* Setting default parameters before ES6
+  //   //Short circuiting
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH112');
+createBooking('LH112', 20, 50);
