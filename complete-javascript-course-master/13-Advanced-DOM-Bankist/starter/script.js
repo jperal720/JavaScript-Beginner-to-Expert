64709c3b28d9725c7bf0a3232713a33a79b94948
jperalgo <jperalgo@sfu.ca>
2022-06-17 19:14:21 -0700

    Learned how to use the -old school method- window.scrollTo() method

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -49,74 +49,99 @@ document.addEventListener('keydown', function (e) {
 //Creating and Inserting elements
 // .insertAdjacentHTML
 
-const header = document.querySelector('.header');
-//!At this point, when creating an element, the element is not in the DOM
-//!Note: We have to add the element to the DOM, ourselves
-const message = document.createElement('section');
-message.classList.add('cookie-message');
-message.textContent = `We used cookies for improved functionality and analytics`;
-message.innerHTML = `We used cookies for improved functionality and analytics. 
-  <button class =
-  "btn btn--close-cookie">Got it!
-  </button>`;
+// const header = document.querySelector('.header');
+// //!At this point, when creating an element, the element is not in the DOM
+// //!Note: We have to add the element to the DOM, ourselves
+// const message = document.createElement('section');
+// message.classList.add('cookie-message');
+// message.textContent = `We used cookies for improved functionality and analytics`;
+// message.innerHTML = `We used cookies for improved functionality and analytics.
+//   <button class =
+//   "btn btn--close-cookie">Got it!
+//   </button>`;
 
-console.log(message);
-// header.prepend(message);
-header.append(message);
-const clone = message.cloneNode(true);
-header.prepend(clone);
-message.style.opacity = 100;
+// console.log(message);
+// // header.prepend(message);
+// header.append(message);
+// const clone = message.cloneNode(true);
+// header.prepend(clone);
+// message.style.opacity = 100;
 
-document.querySelectorAll('.btn--close-cookie').forEach(curr =>
-  curr.addEventListener('click', () => {
-    console.log('remove');
-    console.log(curr.parentElement);
-    curr.parentElement.remove();
-  })
-);
+// document.querySelectorAll('.btn--close-cookie').forEach(curr =>
+//   curr.addEventListener('click', () => {
+//     console.log('remove');
+//     console.log(curr.parentElement);
+//     curr.parentElement.remove();
+//   })
+// );
 
-message.style.backgroundColor = `#37383d`;
-message.style.width = `120%`;
-clone.style.backgroundColor = `#37383d`;
+// message.style.backgroundColor = `#37383d`;
+// message.style.width = `120%`;
+// clone.style.backgroundColor = `#37383d`;
 
-console.log(message.style.width);
-console.log(getComputedStyle(message).height);
-console.log(getComputedStyle(message).color);
+// console.log(message.style.width);
+// console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message).color);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + `px`;
-console.log(getComputedStyle(message).height);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + `px`;
+// console.log(getComputedStyle(message).height);
 
-// document.documentElement.style.setProperty('--color-primary', 'orangered');
+// // document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-//Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.designer);
-console.log(logo.className);
+// //Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.designer);
+// console.log(logo.className);
 
-//Non-standar
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('designer', 'Jonathan');
-console.log(logo.getAttribute('designer'));
-const urlMichael =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Michael_Jackson_Dangerous_World_Tour_1993.jpg/385px-Michael_Jackson_Dangerous_World_Tour_1993.jpg';
-logo.setAttribute('src', urlMichael);
-logo.style.height =
-  Number.parseFloat(getComputedStyle(logo).height, 10) + 200 + 'px';
-logo.style.height =
-  Number.parseFloat(getComputedStyle(logo).width, 10) + 400 + 'px';
+// //Non-standar
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('designer', 'Jonathan');
+// console.log(logo.getAttribute('designer'));
+// const urlMichael =
+//   'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Michael_Jackson_Dangerous_World_Tour_1993.jpg/385px-Michael_Jackson_Dangerous_World_Tour_1993.jpg';
+// logo.setAttribute('src', urlMichael);
+// logo.style.height =
+//   Number.parseFloat(getComputedStyle(logo).height, 10) + 200 + 'px';
+// logo.style.height =
+//   Number.parseFloat(getComputedStyle(logo).width, 10) + 400 + 'px';
 
-//Data attributes
-console.log(logo.dataset.versionNumber);
+// //Data attributes
+// console.log(logo.dataset.versionNumber);
 
-//Classes - how to manipulate them on the DOM.
-//?Don't worry about the details; it is just done to refresh memory
-logo.classList.add('c', 'j');
-logo.classList.remove('d');
-logo.classList.toggle('c');
-logo.classList.contains('d');
+// //Classes - how to manipulate them on the DOM.
+// //?Don't worry about the details; it is just done to refresh memory
+// logo.classList.add('c', 'j');
+// logo.classList.remove('d');
+// logo.classList.toggle('c');
+// logo.classList.contains('d');
 
-//Do not manipulate classes like this //! It will only override the already written ones;
-logo.className = 'jonathan';
+// //Do not manipulate classes like this //! It will only override the already written ones;
+// logo.className = 'jonathan';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (x/y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
+
+  // window.scrollTo(s1coords.left, s1coords.top + window.pageYOffset);
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
