e9b3148a9df0407caf244c21d1270d18ee22bc33
jperalgo <jperalgo@sfu.ca>
2022-06-17 21:21:36 -0700

    Learned alternative ways to create and remove eventListeners

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -23,6 +25,8 @@ const closeModal = function () {
 //   btnsOpenModal[i].addEventListener('click', openModal);
 btnsOpenModal.forEach(curr => curr.addEventListener('click', openModal));
 
+//*Events
+
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -30,6 +34,14 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+btnScrollTo.addEventListener('click', e => {
+  //*element.scrollIntoView() method only works in modern browsers
+
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
 });
 
 //////////////////////////////////////////////////
@@ -121,27 +133,18 @@ document.addEventListener('keydown', function (e) {
 // //Do not manipulate classes like this //! It will only override the already written ones;
 // logo.className = 'jonathan';
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+const h1 = document.querySelector('h1');
 
-btnScrollTo.addEventListener('click', e => {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+const alertFunc = e => {
+  alert(`Mouse has entered`);
 
-  console.log(e.target.getBoundingClientRect());
+  h1.removeEventListener('mouseenter', alertFunc);
+};
 
-  console.log('Current scroll (x/y)', window.pageXOffset, window.pageYOffset);
+h1.addEventListener('mouseenter', alertFunc);
 
-  console.log(
-    document.documentElement.clientWidth,
-    document.documentElement.clientHeight
-  );
+setTimeout(() => h1.removeEventListener('mouseenter', alertFunc), 3000);
 
-  // window.scrollTo(s1coords.left, s1coords.top + window.pageYOffset);
-
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
-});
+// h1.onmouseenter = e => {
+//   alert('you gay');
+// };
